d0fabdf6570bd07337e0a5cb0995bd57a64bc2ea
Andrei.Tokmakov <andtokm@gmail.com>
2026-09-24 20:05:37 +0400

    24_09_2026-002

--- trading_core/tests/test_support/testing.hpp
+++ trading_core/tests/test_support/testing.hpp
@@ -16,7 +16,7 @@ Description : testing.hpp
 
 namespace testing
 {
-   enum class Action
+    enum class Action
     {
         Continue,
         Terminate
@@ -24,142 +24,383 @@ namespace testing
 
     constexpr void performAction(const Action action)
     {
-        if ( Action::Terminate == action)
+        if (Action::Terminate == action)
         {
             std::terminate();
         }
     }
+
+    template<typename Ty>
+    concept Comparable = std::equality_comparable<Ty>;
+
+    template<typename Ty>
+    concept Streamable = requires(std::ostream& stream, const Ty& value)
+    {
+        stream << value;
+    };
+
+    template<typename Ty>
+    concept Ordered = std::totally_ordered<Ty>;
+
+    template<typename Ty>
+    concept FloatingPoint = std::floating_point<Ty>;
 
     constexpr void printLocation(const std::source_location& location)
     {
         std::println(std::cerr, "\tFile: {}\n\tFunction: {}\n\tLine: {}",
-            location.file_name(), location.function_name(), location.line());
+                     location.file_name(), location.function_name(), location.line());
     }
 
-    constexpr void Assert(const bool condition,
-                          const std::string_view message,
-                          const std::source_location& location = std::source_location::current())
+    constexpr void reportFailure(const std::string_view message,
+                                 const Action action,
+                                 const std::source_location location)
     {
-        if (!condition)
-        {
-            std::cerr << "FAILED: " << message << '\n';
-            printLocation(location);
-            std::terminate();
-        }
+        std::println(std::cerr, "{}", message);
+        printLocation(location);
+        performAction(action);
     }
 
-    // TODO: Add concepts to 'Ty'
+    template<Streamable Ty>
+    constexpr void reportComparisonFailure(const std::string_view message,
+                                           const Ty& expected,
+                                           const Ty& actual,
+                                           const Action action,
+                                           const std::source_location location)
+    {
+        std::println(std::cerr, "{}\n\tExpected: {}\n\tActual:   {}",
+                     message, expected, actual);
+        printLocation(location);
+        performAction(action);
+    }
+
     template<typename Ty>
+    constexpr void reportComparisonFailure(const std::string_view message,
+                                           const Ty&,
+                                           const Ty&,
+                                           const Action action,
+                                           const std::source_location location)
+    {
+        reportFailure(message, action, location);
+    }
+
+    template<Comparable Ty>
     constexpr void AssertEqual(const Ty& expected,
                                const Ty& actual,
                                const std::string_view message,
                                const Action action = Action::Terminate,
-                               const std::source_location& location = std::source_location::current())
+                               const std::source_location location = std::source_location::current())
     {
         if (expected != actual)
         {
-            std::println(std::cerr, "{}: {} != {}", message, expected, actual);
-            printLocation(location);
-            performAction(action);
+            reportComparisonFailure(message, expected, actual, action, location);
         }
     }
 
-    // TODO: Add concepts to 'Ty'
-    template<typename Ty>
+    template<Comparable Ty>
     constexpr void AssertEqual(const Ty& expected,
                                const Ty& actual,
                                const Action action = Action::Terminate,
-                               const std::source_location& location = std::source_location::current())
+                               const std::source_location location = std::source_location::current())
     {
         AssertEqual(expected, actual, "Assertion failed:", action, location);
     }
 
-    // TODO: Add concepts to 'Ty'
+    template<Comparable Ty>
+    constexpr void AssertNotEqual(const Ty& expected,
+                                  const Ty& actual,
+                                  const std::string_view message,
+                                  const Action action = Action::Terminate,
+                                  const std::source_location location = std::source_location::current())
+    {
+        if (expected == actual)
+        {
+            reportComparisonFailure(message, expected, actual, action, location);
+        }
+    }
+
+    template<Comparable Ty>
+    constexpr void AssertNotEqual(const Ty& expected,
+                                  const Ty& actual,
+                                  const Action action = Action::Terminate,
+                                  const std::source_location location = std::source_location::current())
+    {
+        AssertNotEqual(expected, actual, "Assertion failed:", action, location);
+    }
+
     template<typename Ty>
     constexpr void AssertNotNull(const Ty* actual,
                                  const std::string_view message,
                                  const Action action = Action::Terminate,
-                                 const std::source_location& location = std::source_location::current())
+                                 const std::source_location location = std::source_location::current())
     {
         if (nullptr == actual)
         {
-            std::println(std::cerr, "{}", message);
-            printLocation(location);
-            performAction(action);
+            reportFailure(message, action, location);
         }
     }
 
-    // TODO: Add concepts to 'Ty'
     template<typename Ty>
     constexpr void AssertNotNull(const Ty* actual,
                                  const Action action = Action::Terminate,
-                                 const std::source_location& location = std::source_location::current())
+                                 const std::source_location location = std::source_location::current())
     {
         AssertNotNull(actual, "Assertion failed (Actual value is null)", action, location);
     }
 
-    // TODO: Add concepts to 'Ty'
     template<typename Ty>
-    constexpr void AssertIsNull(const Ty* actual,
-                                const std::string_view message,
-                                const Action action = Action::Terminate,
-                                const std::source_location& location = std::source_location::current())
+    constexpr void AssertNull(const Ty* actual,
+                              const std::string_view message,
+                              const Action action = Action::Terminate,
+                              const std::source_location location = std::source_location::current())
     {
         if (nullptr != actual)
         {
-            std::println(std::cerr, "{}",message);
-            printLocation(location);
-            performAction(action);
+            reportFailure(message, action, location);
         }
     }
 
-    // TODO: Add concepts to 'Ty'
     template<typename Ty>
-    constexpr void AssertIsNull(const Ty* actual,
-                                const Action action = Action::Terminate,
-                                const std::source_location& location = std::source_location::current())
+    constexpr void AssertNull(const Ty* actual,
+                              const Action action = Action::Terminate,
+                              const std::source_location location = std::source_location::current())
     {
-        AssertIsNull(actual, "Assertion failed (Actual value is not null)", action, location);
+        AssertNull(actual, "Assertion failed (Actual value is not null)", action, location);
     }
 
     constexpr void AssertTrue(const bool condition,
                               const std::string_view message,
                               const Action action = Action::Terminate,
-                              const std::source_location& location = std::source_location::current())
+                              const std::source_location location = std::source_location::current())
     {
         if (!condition)
         {
-            std::println(std::cerr, "{}", message);
-            printLocation(location);
-            performAction(action);
+            reportFailure(message, action, location);
         }
     }
 
     constexpr void AssertTrue(const bool condition,
                               const Action action = Action::Terminate,
-                              const std::source_location& location = std::source_location::current())
+                              const std::source_location location = std::source_location::current())
     {
-        AssertTrue(condition, "Condition is False (True expected):", action, location);
+        AssertTrue(condition, "Condition is False (True expected)", action, location);
+    }
+
+    constexpr void Assert(const bool condition,
+                 const std::string_view message,
+                 const std::source_location& location = std::source_location::current())
+    {
+        AssertTrue(condition, message, Action::Terminate, location);
     }
 
     constexpr void AssertFalse(const bool condition,
                                const std::string_view message,
                                const Action action = Action::Terminate,
-                               const std::source_location& location = std::source_location::current())
+                               const std::source_location location = std::source_location::current())
     {
         if (condition)
         {
-            std::println(std::cerr, "{}", message);
-            printLocation(location);
-            performAction(action);
+            reportFailure(message, action, location);
         }
     }
 
     constexpr void AssertFalse(const bool condition,
                                const Action action = Action::Terminate,
-                               const std::source_location& location = std::source_location::current())
+                               const std::source_location location = std::source_location::current())
     {
-        AssertFalse(condition, "Condition is True (False expected):", action, location);
+        AssertFalse(condition, "Condition is True (False expected)", action, location);
+    }
+
+    template<Ordered Ty>
+    constexpr void AssertGreater(const Ty& actual,
+                                 const Ty& expected,
+                                 const std::string_view message,
+                                 const Action action = Action::Terminate,
+                                 const std::source_location location = std::source_location::current())
+    {
+        if (actual <= expected)
+        {
+            reportComparisonFailure(message, expected, actual, action, location);
+        }
+    }
+
+    template<Ordered Ty>
+    constexpr void AssertGreater(const Ty& actual,
+                                 const Ty& expected,
+                                 const Action action = Action::Terminate,
+                                 const std::source_location location = std::source_location::current())
+    {
+        AssertGreater(actual, expected, "Assertion failed:", action, location);
+    }
+
+    template<Ordered Ty>
+    constexpr void AssertGreaterEqual(const Ty& actual,
+                                      const Ty& expected,
+                                      const std::string_view message,
+                                      const Action action = Action::Terminate,
+                                      const std::source_location location = std::source_location::current())
+    {
+        if (actual < expected)
+        {
+            reportComparisonFailure(message, expected, actual, action, location);
+        }
+    }
+
+    template<Ordered Ty>
+    constexpr void AssertGreaterEqual(const Ty& actual,
+                                      const Ty& expected,
+                                      const Action action = Action::Terminate,
+                                      const std::source_location location = std::source_location::current())
+    {
+        AssertGreaterEqual(actual, expected, "Assertion failed:", action, location);
+    }
+
+    template<Ordered Ty>
+    constexpr void AssertLess(const Ty& actual,
+                              const Ty& expected,
+                              const std::string_view message,
+                              const Action action = Action::Terminate,
+                              const std::source_location location = std::source_location::current())
+    {
+        if (actual >= expected)
+        {
+            reportComparisonFailure(message, expected, actual, action, location);
+        }
+    }
+
+    template<Ordered Ty>
+    constexpr void AssertLess(const Ty& actual,
+                              const Ty& expected,
+                              const Action action = Action::Terminate,
+                              const std::source_location location = std::source_location::current())
+    {
+        AssertLess(actual, expected, "Assertion failed:", action, location);
+    }
+
+    template<Ordered Ty>
+    constexpr void AssertLessEqual(const Ty& actual,
+                                   const Ty& expected,
+                                   const std::string_view message,
+                                   const Action action = Action::Terminate,
+                                   const std::source_location location = std::source_location::current())
+    {
+        if (actual > expected)
+        {
+            reportComparisonFailure(message, expected, actual, action, location);
+        }
+    }
+
+    template<Ordered Ty>
+    constexpr void AssertLessEqual(const Ty& actual,
+                                   const Ty& expected,
+                                   const Action action = Action::Terminate,
+                                   const std::source_location location = std::source_location::current())
+    {
+        AssertLessEqual(actual, expected, "Assertion failed:", action, location);
+    }
+
+    template<FloatingPoint Ty>
+    constexpr void AssertNear(const Ty& expected,
+                              const Ty& actual,
+                              const Ty& tolerance,
+                              const std::string_view message,
+                              const Action action = Action::Terminate,
+                              const std::source_location location = std::source_location::current())
+    {
+        if (std::abs(expected - actual) > tolerance)
+        {
+            reportComparisonFailure(message, expected, actual, action, location);
+        }
+    }
+
+    template<FloatingPoint Ty>
+    constexpr void AssertNear(const Ty& expected,
+                              const Ty& actual,
+                              const Ty& tolerance,
+                              const Action action = Action::Terminate,
+                              const std::source_location location = std::source_location::current())
+    {
+        AssertNear(expected, actual, tolerance, "Assertion failed:", action, location);
+    }
+
+    template<typename Ty>
+    concept ExpectedType = requires
+    {
+        typename Ty::value_type;
+        typename Ty::error_type;
+    };
+
+    template<ExpectedType Ty>
+    constexpr void AssertExpected(const Ty& actual,
+                                  const std::string_view message,
+                                  const Action action = Action::Terminate,
+                                  const std::source_location location = std::source_location::current())
+    {
+        if (!actual.has_value())
+        {
+            reportFailure(message, action, location);
+        }
+    }
+
+    template<ExpectedType Ty>
+    constexpr void AssertExpected(const Ty& actual,
+                                  const Action action = Action::Terminate,
+                                  const std::source_location location = std::source_location::current())
+    {
+        AssertExpected(actual, "Assertion failed (Expected value, but got error)", action, location);
+    }
+
+    template<ExpectedType Ty>
+        requires Comparable<typename Ty::value_type>
+    constexpr void AssertExpected(const Ty& actual,
+                                  const typename Ty::value_type& expected,
+                                  const std::string_view message,
+                                  const Action action = Action::Terminate,
+                                  const std::source_location location = std::source_location::current())
+    {
+        if (!actual.has_value())
+        {
+            reportFailure(message, action, location);
+            return;
+        }
+
+        AssertEqual(expected, actual.value(), message, action, location);
+    }
+
+    template<ExpectedType Ty>
+        requires Comparable<typename Ty::value_type>
+    constexpr void AssertExpected(const Ty& actual,
+                                  const typename Ty::value_type& expected,
+                                  const Action action = Action::Terminate,
+                                  const std::source_location location = std::source_location::current())
+    {
+        AssertExpected(actual, expected, "Assertion failed:", action, location);
+    }
+
+    template<ExpectedType Ty>
+        requires Comparable<typename Ty::error_type>
+    constexpr void AssertExpectedError(const Ty& actual,
+                                       const typename Ty::error_type& expected,
+                                       const std::string_view message,
+                                       const Action action = Action::Terminate,
+                                       const std::source_location location = std::source_location::current())
+    {
+        if (actual.has_value())
+        {
+            reportFailure(message, action, location);
+            return;
+        }
+
+        AssertEqual(expected, actual.error(), message, action, location);
+    }
+
+    template<ExpectedType Ty>
+        requires Comparable<typename Ty::error_type>
+    constexpr void AssertExpectedError(const Ty& actual,
+                                       const typename Ty::error_type& expected,
+                                       const Action action = Action::Terminate,
+                                       const std::source_location location = std::source_location::current())
+    {
+        AssertExpectedError(actual, expected, "Assertion failed:", action, location);
     }
 }
 
